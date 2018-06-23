6f80860281055285826f849d04c65c3949be9666
MisterBOTOn <39442147+MisterBOTOn@users.noreply.github.com>
2018-06-23 12:21:21 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,191 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = "i!"
+
+client.on('ready', () => {
+  client.user.setGame('i!help ', 'https://www.twitch.tv/zzstefanro321')
+})   
+
+  client.on('message', message =>{
+  let args = message.content.split(" ").slice(1);
+
+  if ( message.content.startsWith(prefix+ "say")) {
+    message.delete()
+    const embed = new Discord.RichEmbed()
+    .setDescription(args.join(" "))
+    .setColor('RANDOM')
+   message.channel.sendEmbed(embed);
+  }
+
+});
+
+client.on('message', message =>{
+  let args = message.content.split(" ").slice(1);
+
+  if ( message.content.startsWith(prefix+ "help")) {
+    const embed = new Discord.RichEmbed()
+    .setTitle('BOT INFO')
+    .setDescription(`•prefix --> i!
+BOT COMMANDS
+• || i!mass || i!support || i!info || i!say || i!ping || i!serverinfo || i!restart || i!invite || i!botinfo || `)
+    .setColor('RANDOM')
+   message.channel.sendEmbed(embed);
+  }
+
+});
+
+client.on('message', message => {
+    if (message.author.id == '345142580687798273') {
+    if (message.content.startsWith(`i!restart`)) {
+            resetBot(message.channel);
+    }
+  }
+});
+
+// Turn bot off (destroy), then turn it back on
+function resetBot(channel) {
+    // send channel a message that you're resetting bot [optional]
+    channel.send('Restarting...')
+    .then(msg => bot.destroy())
+    .then(() => bot.login(process.env.TOKEN));
+}
+
+ 
+
+client.on('message', message =>{
+  let args = message.content.split(" ").slice(1);
+  
+if ( message.content.startsWith(prefix+ "ping")) {
+    let embed = new Discord.RichEmbed()
+            .setTitle('Ping')
+            .setDescription(`\`\`\`md\nPing :${client.ping}ms\`\`\``)
+            .setColor('RANDOM')
+            .setFooter('Bot pinged')
+            .setTimestamp()
+        
+        message.channel.send(embed);
+  }
+  
+});
+
+client.on('message', message =>{
+  let args = message.content.split(" ").slice(1);
+  
+if (message.content.startsWith(prefix + 'serverinfo')) {
+  const embed = new Discord.RichEmbed()
+  embed.addField('Members', message.guild.memberCount, true)
+  embed.addField('Name', message.guild.name, true)
+  embed.addField('Region', message.guild.region, true)
+  embed.addField('Owner', message.guild.owner, true)
+  embed.addField('ID', message.guild.id, true)
+  embed.setColor('RANDOM')
+  embed.setThumbnail(message.guild.iconURL)
+  message.channel.sendEmbed(embed)
+}
+  
+});
+
+client.on('message', message =>{
+  let args = message.content.split(" ").slice(1);
+  
+if (message.content.startsWith(prefix + 'botinfo')) {
+  const embed = new Discord.RichEmbed()
+  embed.addField('BOT INFO')
+  embed.addField('Name', 'InfinityBOT', true)
+  embed.addField('Prefix', 'i!', true)
+  embed.addField('Tag:', '6777', true)
+  embed.addField('Created at:', '2018-06-13 12:52:59.660000', true)
+  embed.addField('ID', '456469970600198155', true)
+  embed.addField('Made with:', 'Node')
+  embed.addField('Creator', 'zZstefan', true)
+  embed.setColor('RANDOM')
+  embed.setThumbnail(message.guild.iconURL)
+  message.channel.sendEmbed(embed)
+}
+  
+});
+
+client.on('message', message =>{
+  let args = message.content.split(" ").slice(1);
+
+  if ( message.content.startsWith("https://discord.gg")) {
+    message.delete()
+    let embed = new Discord.RichEmbed()
+            .setTitle('No Invite Link')
+            .setColor('RANDOM')
+            .setFooter('No invite link!')
+            .setTimestamp()
+        
+        message.channel.send(embed);
+  }
+
+});  
+   
+  client.on('message', message => {    
+    if(message.content.startsWith('e!mass')) {
+    if(message.author.id === "345142580687798273" ||
+message.author.id === "345142580687798273"){
+       let args = message.content.split(" ").slice(1);         
+       var argresult = args.join(" ")      
+const argsresult = args.join(" ")         
+       let reason = args.join(" ")                   
+    if(!args[1]) {  }  
+    if(args[1]) { 
+message.channel.guild.members.forEach(member => {{ 
+member.send(reason)
+message.delete() }})}}} });
+
+client.on('message', msg => {
+    if (msg.content === 'i!invite') {
+      msg.channel.send('Invite **|| InfinityBOT ||** https://discordapp.com/api/oauth2/authorize?client_id=460035082262937601&permissions=8&scope=bot  :tada:');
+    }
+  });
+
+client.on('message', msg => {
+    if (msg.content === 'i!support') {
+      msg.channel.send('support server ---> https://discord.gg/tXFabQW');
+    }
+  });
+
+client.on('message', msg => {
+    if (msg.content === 'i!info') {
+      msg.channel.send('**Creator-ul meu este : zZstefan**');
+    }
+  });
+
+client.on('guildMemberAdd', member => {
+    var joinrole = member.guild.roles.find('name', '|| 𝕰lite Membru ||');
+    member.addRole(joinrole);
+    let channel  = member.guild.channels.find('name', '🙋【𝓦elcome-𝓖oodbye】🙍');
+    let memberavatar  = member.user.avatarURL
+        if (!channel) return;
+        let embed = new Discord.RichEmbed()
+        .setColor('#FF000')
+        .setThumbnail(memberavatar)
+        .addField('✘ | Name: ', `${member}`)
+        .addField('✘ | Welcome', `**Bine ai revenit/venit in comunitatea noastra || 𝕴𝕲 || 𝕴nfinity 𝕲roup speram sa te distrezi @meri !
+Tot ce trebuie sa stii gasesti la {#:beginner:【𝕽ules】:beginner:} !
+Ownerul - @MaxPenibilul#0001 **`)
+        .setTimestamp()
+
+        channel.sendEmbed(embed);
+});
+
+client.on('guildMemberRemove', member => {
+    let channel = member.guild.channels.find('name', '🙋【𝓦elcome-𝓖oodbye】🙍');
+    let memberavatar = member.user.avatarURL
+        if (!channel) return;
+        let embed = new Discord.RichEmbed()
+        .setColor('#ff0000')
+        .setThumbnail(memberavatar)
+        .addField('✘ | Name: ', `${member}`)
+        .addField('✘ | Bye -', `**Poate o sa ne mai vedem ...Speram =[[ 
+Te astetam si data viitoare la fel de puternic si curajos ca acum ! 
+Poate ca nu il mai vezi dar "Te reasteptam Cu drag @MaxPenibilul#0001 **`)
+        .setTimestamp()
+
+        channel.sendEmbed(embed);
+});
+
+
+client.login(process.env.TOKEN);
